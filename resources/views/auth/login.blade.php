5dfbb9f2e813f495fac72bf016ee7f417a2a6870
Muthia Zhafira Sahnah <134127109+muthiazs@users.noreply.github.com>
2024-10-27 15:34:45 +0700

    27 okt resolve conflict abis merge

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,30 +8,31 @@
                 <div class="card-header text-center">Login</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login.post') }}">
-                    @csrf
-                    <div class="form-group mb-3">
-                        <label class="form-label">Email:</label>
-                        <input type="text" placeholder="email@gmail.com" 
-                               id="email" class="form-control" name="email"
-                               required autofocus>
-                        @if ($errors->has('email'))
-                            <span class="text-danger">
-                                {{$errors->first('email')}}
-                            </span>
-                        @endif
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="password" class="form-label">Password:</label>
-                        <input type="password" class="form-control" id="password"
-                               placeholder="****" name="password" required>
-                        @if ($errors->has('password'))
-                            <span class="text-danger">
-                                {{$errors->first('password')}}
-                            </span>
-                        @endif
-                    </div>
-                    <button type="submit" class="btn btn-primary" name="submit" value="submit">Masuk</button>
-                </form>
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="email" class="form-label">Email:</label> <!-- Tambahkan for="email" -->
+                            <input type="text" placeholder="email@gmail.com" 
+                                   id="email" class="form-control" name="email"
+                                   required autofocus autocomplete="username">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">
+                                    {{$errors->first('email')}}
+                                </span>
+                            @endif
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password" class="form-label">Password:</label>
+                            <input type="password" class="form-control" id="password"
+                                   placeholder="****" name="password" required
+                                   autocomplete="current-password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">
+                                    {{$errors->first('password')}}
+                                </span>
+                            @endif
+                        </div>
+                        <button type="submit" class="btn btn-primary" name="submit" value="submit">Masuk</button>
+                    </form>
                 </div>
             </div>
         </div>
