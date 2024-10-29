fb260252980ede8c64e953dffd2210dc42ce4bba
Muthia Zhafira Sahnah <134127109+muthiazs@users.noreply.github.com>
2024-10-29 20:14:14 +0700

    perbarui font dashboard jadi poppins

--- resources/views/dashboardDosen.blade.php
+++ resources/views/dashboardDosen.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <style>
         /* Buat Side bar */
         .sidebar {
