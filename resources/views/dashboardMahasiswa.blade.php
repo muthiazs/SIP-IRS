cdb7f0ca122412fb01ad5534da3d9da2bb80d4c9
unknown <nadivaaulia2512@gmail.com>
2024-11-06 13:00:14 +0700

    push nadiva 06/11/24 13.00

--- resources/views/dashboardMahasiswa.blade.php
+++ resources/views/dashboardMahasiswa.blade.php
@@ -299,7 +299,7 @@
                         @if($data['status']['irs'] === 'ditolak')
                             <div class="alert alert-danger mb-0">
                                 Isian Rencana Studi Ditolak
-                                <a href="#" class="btn btn-danger mt-2">Lihat Detail</a>
+                                <a href="/pengisianIRS" class="btn btn-danger mt-2">Lihat Detail</a>
                             </div>
                         @endif
                     </div>
