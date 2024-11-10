58596087d609656c746e6e12adbbe9ce1e58c720
capinowo <alyasafina507@gmail.com>
2024-11-10 13:50:51 +0700

    re: tiada yang salah hanya aku manusia bodoh

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -38,30 +38,32 @@
 
     @elseif ($userRole == 'mahasiswa')
         <!-- SIDEBAR MAHASISWA -->
-        <div class="text-center mb-4">
-            <div class="profile-img mb-3">
-                <span class="material-icons" style="font-size: 48px; color: var(--primary-color)">person</span>
+        @if (@isset($mahasiswa))
+            <div class="text-center mb-4">
+                <div class="profile-img mb-3">
+                    <span class="material-icons" style="font-size: 48px; color: var(--primary-color)">person</span>
+                </div>
+                <h2 class="fs-4 fw-bold">{{ $mahasiswa->nama_mhs }}</h2>
+                <p class="small opacity-75">NIM. {{ $mahasiswa->nim }}</p>
+                <p class="small opacity-75">Mahasiswa<br>Program Studi S1 {{ $mahasiswa->prodi_nama }}<br> Fakultas Sains dan Matematika</p>
+                <p class="small opacity-75">Dosen Wali: {{ $mahasiswa->nama_doswal }}<br>NIP. {{ $mahasiswa->nip }}</p>
             </div>
-            <h2 class="fs-4 fw-bold">{{ $mahasiswa->nama_mhs }}</h2>
-            <p class="small opacity-75">NIM. {{ $mahasiswa->nim }}</p>
-            <p class="small opacity-75">Mahasiswa<br>Program Studi S1 {{ $mahasiswa->prodi_nama }}<br> Fakultas Sains dan Matematika</p>
-            <p class="small opacity-75">Dosen Wali: {{ $mahasiswa->nama_doswal }}<br>NIP. {{ $mahasiswa->nip }}</p>
-        </div>
 
-        <nav class="nav flex-column gap-2 mb-4">
-            <a href="#" class="nav-link active rounded d-flex align-items-center">
-                <span class="material-icons me-3">home</span>
-                Beranda
-            </a>
-            <a href="#" class="nav-link rounded d-flex align-items-center">
-                <span class="material-icons me-3">description</span>
-                Rencana Studi
-            </a>
-            <a href="#" class="nav-link rounded d-flex align-items-center">
-                <span class="material-icons me-3">assessment</span>
-                Hasil Studi
-            </a>
-        </nav>
+            <nav class="nav flex-column gap-2 mb-4">
+                <a href="#" class="nav-link active rounded d-flex align-items-center">
+                    <span class="material-icons me-3">home</span>
+                    Beranda
+                </a>
+                <a href="#" class="nav-link rounded d-flex align-items-center">
+                    <span class="material-icons me-3">description</span>
+                    Rencana Studi
+                </a>
+                <a href="#" class="nav-link rounded d-flex align-items-center">
+                    <span class="material-icons me-3">assessment</span>
+                    Hasil Studi
+                </a>
+            </nav>
+            @endif
 
     @elseif ($userRole == 'dosen' && $userRole2 == 'kaprodi' && $roleSelected == 'kaprodi')
         <!-- SIDEBAR KAPRODI -->
