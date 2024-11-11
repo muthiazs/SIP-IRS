808ab959a935436f5886d3e76cffa5d4faad6deb
Muthia Zhafira Sahnah <134127109+muthiazs@users.noreply.github.com>
2024-11-11 11:10:19 +0700

    benerin jadwal kuliah

--- database/seeders/JadwalKuliahSeeder.php
+++ database/seeders/JadwalKuliahSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class JadwalKuliahSeeder extends Seeder
 {
@@ -36,15 +35,19 @@ class JadwalKuliahSeeder extends Seeder
         ];
 
         // Data dosen
-        $dosen = [5, 6, 7, 8, 9, 10, 11, 12, 13,14,15];
+        $dosen = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
         // Data ruangan
         $ruangan = range(1, 23);
 
-        // Hari dan jam perkuliahan
+        // Hari perkuliahan
         $hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
+
+        // Jam mulai dan jam selesai
         $jam_mulai = ['08:00:00', '10:00:00', '13:00:00', '15:00:00'];
         $jam_selesai = ['10:00:00', '12:00:00', '15:00:00', '17:00:00'];
+
+        // Kelas
         $kelas = ['A', 'B', 'C'];
 
         // ID Periode Akademik
@@ -53,10 +56,13 @@ class JadwalKuliahSeeder extends Seeder
             ['id_periode' => '24251', 'nama_periode' => 'Semester Akademik 2024/2025 Ganjil', 'tahun_mulai' => '2024', 'tahun_selesai' => '2025', 'jenis' => 'ganjil']
         ];
 
-          // Insert jadwal kuliah
-          foreach ($matkul as $mat) {
+        // Insert jadwal kuliah
+        foreach ($matkul as $mat) {
             // Randomly select a periode
             $selectedPeriode = $periodes[array_rand($periodes)];
+
+            // Randomly select jam mulai dan jam selesai sesuai dengan pola yang diinginkan
+            $indexJam = array_rand($jam_mulai);
 
             DB::table('jadwal_kuliah')->insert([
                 'kode_matkul' => $mat['kode_matkul'],
@@ -64,15 +70,14 @@ class JadwalKuliahSeeder extends Seeder
                 'id_dosen' => $dosen[array_rand($dosen)],
                 'id_ruang' => $ruangan[array_rand($ruangan)],
                 'hari' => $hari[array_rand($hari)],
-                'jam_mulai' => $jam_mulai[array_rand($jam_mulai)],
-                'jam_selesai' => $jam_selesai[array_rand($jam_selesai)],
+                'jam_mulai' => $jam_mulai[$indexJam],
+                'jam_selesai' => $jam_selesai[$indexJam],
                 'kelas' => $kelas[array_rand($kelas)],
                 'semester' => $mat['semester'],
                 'id_periode' => $selectedPeriode['id_periode'],
-                'created_at' =>now(),
-                'updated_at' =>now(),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
         }
     }
 }
-
