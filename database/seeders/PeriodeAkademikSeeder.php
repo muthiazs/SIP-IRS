a80021b68a24dfa22efc6df9caceb911d489639a
Muthia Zhafira Sahnah <134127109+muthiazs@users.noreply.github.com>
2024-12-06 19:48:29 +0700

    ubah periode biar gk bolak balik ke settinh:

--- database/seeders/PeriodeAkademikSeeder.php
+++ database/seeders/PeriodeAkademikSeeder.php
@@ -14,15 +14,15 @@ class PeriodeAkademikSeeder extends Seeder
             [
                 'id_periode' => '23242',
                 'nama_periode' => 'Semester Akademik 2023/2024 Genap',
-                'tahun_mulai' => '2024-09-10 00:00:00',
-                'tahun_selesai' => '2024-06-10 23:59:59',
+                'tahun_mulai' => '2024-08-03 00:00:00',
+                'tahun_selesai' => '2024-12-31 23:59:59',
                 'jenis' => 'genap',
             ],
             [
                 'id_periode' => '24251',
                 'nama_periode' => 'Semester Akademik 2024/2025 Ganjil',
-                'tahun_mulai' => '2024-07-10 00:00:00', // Updated start date
-                'tahun_selesai' => '2025-01-10 23:59:59',
+                'tahun_mulai' => '2024-09-10 00:00:00', // Updated start date
+                'tahun_selesai' => '2025-06-10 23:59:59',
                 'jenis' => 'ganjil',
             ]
         ];
