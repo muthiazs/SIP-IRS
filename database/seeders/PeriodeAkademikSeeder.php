49bea08dddf6bd308e87da0c6465770c3f7c5752
Muthia Zhafira Sahnah <134127109+muthiazs@users.noreply.github.com>
2024-12-07 15:47:02 +0700

    1. ngubah tombol kembali jadi disable kalo di periode batal irs ✅ 2. untuk melakukan itu harus ubah periode batal irs karena tadi aku samain ✅ 2. bikin seeder jadwal yang kuotanya 2 (basis data aja ya soalnya ak cinta basis data) ✅✅

--- database/seeders/PeriodeAkademikSeeder.php
+++ database/seeders/PeriodeAkademikSeeder.php
@@ -9,29 +9,29 @@ use Carbon\Carbon;
 class PeriodeAkademikSeeder extends Seeder
 {
     public function run()
-    {
-        // Ambil tanggal saat ini
-        $currentDate = Carbon::now();
-        
-        // Data periode akademik
-        $periode = [
-            [
-                'id_periode' => '23242',
-                'nama_periode' => 'Semester Akademik 2023/2024 Genap',
-                'tahun_mulai' => '2023-08-03 00:00:00',
-                'tahun_selesai' => '2023-12-03 23:59:59',
-                'jenis' => 'genap',
-            ],
-            [
-                'id_periode' => '24251',
-                'nama_periode' => 'Semester Akademik 2024/2025 Ganjil',
-                'tahun_mulai' => $currentDate->subWeek(1)->toDateTimeString(), // 2 minggu dari sekarang + 1 tahun
-                'tahun_selesai' => $currentDate->copy()->addWeeks(3)->addYear()->toDateTimeString(), // 3 minggu dari sekarang + 1 tahun
-                'jenis' => 'ganjil',
-            ]
-        ];
+{
+    // Ambil tanggal saat ini
+    $currentDate = Carbon::now();
+    
+    // Data periode akademik
+    $periode = [
+        [
+            'id_periode' => '23242',
+            'nama_periode' => 'Semester Akademik 2023/2024 Genap',
+            'tahun_mulai' => '2023-08-03 00:00:00',
+            'tahun_selesai' => '2023-12-03 23:59:59',
+            'jenis' => 'genap',
+        ],
+        [
+            'id_periode' => '24251',
+            'nama_periode' => 'Semester Akademik 2024/2025 Ganjil',
+            'tahun_mulai' => $currentDate->addMonths(6)->toDateTimeString(), // 6 bulan dari sekarang
+            'tahun_selesai' => $currentDate->copy()->addMonths(6)->toDateTimeString(), // 6 bulan dari sekarang
+            'jenis' => 'ganjil',
+        ]
+    ];
 
-        // Insert data periode akademik
-        DB::table('periode_akademik')->insert($periode);
-    }
+    // Insert data periode akademik
+    DB::table('periode_akademik')->insert($periode);
+}
 }
