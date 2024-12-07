182088c917a759b6814b48bc75069ecd503f1bcb
capinowo <alyasafina507@gmail.com>
2024-12-07 07:17:21 +0700

    typo controller lagi akawokawokwa

--- app/Http/Controllers/KaprodiControler.php
+++ app/Http/Controllers/KaprodiControler.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
-class KaprodiController extends Controller
+class KaprodiControler extends Controller
 {
     public function DashboardKaprodi()
     {
