462b74718bbe8c9d08c92017dbfa9a2004f580e0
aurelliatirza <tirza.aurelliawijaya@gmail.com>
2024-12-12 19:22:54 +0700

    setujui Jadwal belom bisa

--- routes/web.php
+++ routes/web.php
@@ -149,6 +149,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/setujui-semua-ruang', [DekanController::class, 'setujuiSemuaRuang'])->name('setujui.semua.ruang');
     Route::post('/tolak-ruang/{id_ruang}', [DekanController::class, 'tolakRuang'])->name('tolak.ruang');    
     Route::get('/dekan_PersetujuanJadwal', [DekanController::class, 'PersetujuanJadwal'])->name('dekan_PersetujuanJadwal');
+    Route::post('/jadwal/setujui', [JadwalController::class, 'setujuiJadwal'])->name('jadwal.setujui');
     Route::get('/pembagianruang', [BAK_PembagianruangController::class, 'index'])->name('pembagianruang');
     Route::get('/bak_PembagianRuang', [BAK_PembagianruangController::class, 'indexPembagianRuang'])->name('bak_PembagianRuang');
     Route::post('/ruang/cancel', [BAK_PembagianruangController::class, 'cancelAlokasiRuang'])->name('cancel.ruang');
